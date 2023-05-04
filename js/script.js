58b93477e93b46c1eb315e3d1b1580c61cba50f2
totarochristian <totaro.christian.18@gmail.com>
2023-05-04 10:07:10 +0200

    Get the main score result element and set the value using the data passed

--- js/script.js
+++ js/script.js
@@ -4,11 +4,15 @@ fetch('../data.json')
 .then((response) => response.json())
 .then((data) => {
   console.log(data);
+  let scoreTot = 0;
   data.forEach(element => {
     var container = document.getElementById(element.category.toLowerCase());
     var img = container.getElementsByClassName("icon")[0];
     img.src = element.icon;
     var score = container.getElementsByClassName("score")[0];
     score.textContent = element.score;
+    scoreTot += element.score;
   });
+  var mainScoreResult = document.getElementById("resultSection").getElementsByClassName("score")[0];
+  mainScoreResult.textContent = Math.round(scoreTot / data.length);
 });
